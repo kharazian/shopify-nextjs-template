879d7ecb8d0b4410c97fedbc375aa702fee8df31
Kharazian <AmirKharazian@gmail.com>
2025-10-09 14:50:05 -0400

    Integrate Polaris Web Components via CDN for App Home UI

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="shopify-api-key" content={process.env.SHOPIFY_API_KEY} />
+        <link rel="icon" href="/favicon.ico" />
+        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
+        <script src="https://cdn.shopify.com/shopifycloud/polaris.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
